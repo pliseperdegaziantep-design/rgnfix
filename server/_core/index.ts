17a203d86c450c7ac49e8b9f2910e55993c6702d
pliseperdegaziantep-design <pliseperdegaziantep@gmail.com>
2026-07-18 11:01:56 -0700

    Register secure admin order routes

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -5,6 +5,7 @@ import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { registerOAuthRoutes } from "./oauth";
 import { registerStorageProxy } from "./storageProxy";
 import { registerLocalAuthRoutes } from "./localAuth";
+import { registerAdminRoutes } from "./adminRoutes";
 import { appRouter } from "../routers";
 import { createContext } from "./context";
 import { serveStatic, setupVite } from "./vite";
@@ -22,6 +23,7 @@ async function startServer() {
 
   registerStorageProxy(app);
   registerLocalAuthRoutes(app);
+  registerAdminRoutes(app);
   registerOAuthRoutes(app);
 
   app.use(
